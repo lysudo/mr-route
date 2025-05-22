ac1460103f81895f5f08536774c77077ae9f8245
sudoly <hamza.musaffah@gmail.com>
2025-05-22 03:08:12 +0200

    initial SPA structure

--- server.py
+++ server.py
@@ -3,10 +3,14 @@ from routes import list_interfaces, get_game_prefixes, apply_routes
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
+    return render_template("pages/home.html")
+
+@app.route('/pages/games')
+def games_routes():
     games = {"Riot":"6507"}
-    return render_template("index.html", games=games, interfaces=list_interfaces())
+    return render_template("pages/games.html", games=games, interfaces=list_interfaces())
 
 @app.route('/apply', methods=['POST'])
 def apply():
@@ -18,3 +22,6 @@ def apply():
     success = apply_routes(prefixes, gateway)
 
     return render_template("apply.html", success=success, prefixes=prefixes)
+
+if __name__ == "__main__":
+    app.run(debug=True)
